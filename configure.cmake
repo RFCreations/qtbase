7739afea4bf6b636c1d261daac2669782f265824
Patrick Stewart <patstew@gmail.com>
2025-06-20 01:34:08 +0100

    Add option to statically link compiler runtime libraries
    On MSVC we use the 'Hybrid CRT' method to avoid linking the UCRT, which is included in Win10+.
    This avoids the need to ship VC redistributables on Windows.
    
    Change-Id: Id40d9d911485ae1f1d336a7869c3bdb54272b621

--- configure.cmake
+++ configure.cmake
@@ -1118,6 +1118,11 @@ qt_feature("static_runtime"
 )
 qt_feature_config("static_runtime" QMAKE_PUBLIC_CONFIG)
 qt_feature_config("static_runtime" QMAKE_PUBLIC_QT_CONFIG)
+qt_feature("static_compiler_runtime" PUBLIC
+    LABEL "Statically link the C++ library and compiler runtime, but not libc"
+    AUTODETECT OFF
+    CONDITION NOT QT_FEATURE_static_runtime AND (WIN32 OR LINUX)
+)
 qt_feature("dlopen" PRIVATE
     LABEL "dlopen()"
     CONDITION UNIX AND NOT INTEGRITY
